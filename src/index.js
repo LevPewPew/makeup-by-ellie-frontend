f5454349fc8ad80632831df7dd165741277d1a2c
LevPewPew <levtoth1988@gmail.com>
2020-01-16 08:01:09 +1100

    forgot to remove SandboxApp reference

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-import SandboxApp from './components/SandboxApp.jsx';
 
-ReactDOM.render(<SandboxApp />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
