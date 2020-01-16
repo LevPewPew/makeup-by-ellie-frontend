4422fe88800ff0f754f290af18580c67c6335f4b
LevPewPew <levtoth1988@gmail.com>
2020-01-16 21:22:01 +1100

    commit so that i can merge latest master

---
+++ src/components/Service.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+function Service(props) {
+  const onAdminDash = useSelector((state) => state.adminDashReducer.onAdminDash);
+  const { title, description, imageUrl } = props;
+
+  return (
+    <div className="Service">
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <img src={imageUrl}/>
+      <div className="crud-per-doc">
+        <button className="edit">Edit</button>
+        <button className="delete">Delete</button>
+      </div>
+    </div>
+  )
+}
---
+++ src/reducers/servicesReducer.js
@@ -0,0 +1,48 @@
+// DUMMY remove later
+const dummyData = [
+  {
+    "_id": "5e1e27590a9dee300d8c85d8",
+    "title": "editorial",
+    "description": "test",
+    "imageUrl": "test",
+    "createdAt": "2020-01-14T20:40:57.092Z",
+    "updatedAt": "2020-01-14T20:40:57.092Z",
+    "__v": 0
+  },
+  {
+    "_id": "5e1e29c33a090432523f104e",
+    "category": "editorial",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRPSnykjoosi4LXvLBhuPIl3JWNjHi2cfa-wt0O1gODuB1Gw0vo",
+    "createdAt": "2020-01-14T20:51:15.329Z",
+    "updatedAt": "2020-01-14T20:51:15.329Z",
+    "__v": 0
+  },
+  {
+    "_id": "5e1e316a0eb8a8367d7f7fef",
+    "category": "wedding",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQyi1p65bhsGvTa4CQ837kzNpGYiN5P9hFNFCARliwvL17egpHr",
+    "createdAt": "2020-01-14T21:23:54.913Z",
+    "updatedAt": "2020-01-14T21:23:54.913Z",
+    "__v": 0
+  }
+]
+
+const initialState = {
+  servicesData: dummyData
+};
+
+function portfolioReducer(state = initialState, action) {
+  let newState = {};
+
+  switch(action.type) {
+    case 'UPDATE_SERVICES_DATA':
+      newState = {...state, servicesData: action.newServicesData};
+      break;
+    default:
+      newState = {...state};
+  }
+
+  return newState;
+}
+
+export default servicesReducer;
