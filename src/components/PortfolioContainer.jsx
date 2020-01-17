2bac71849048688a4a6f6ac4d3002d7687b9e648
LevPewPew <levthedev@protonmail.com>
2020-01-17 16:12:35 +1100

    added a Work component, rejigged WorksContainer by renaming as well as rendering a .map of the new Work component

---
+++ src/components/PortfolioContainer.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Axios from 'axios';
+import Work from './Work';
+
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
+function WorksContainer() {
+  const portfolioData = useSelector((state) => state.portfolioReducer.portfolioData);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      let res = await Axios.get(`${backendUrl}/portfolio`);
+      console.log(res.data);
+      dispatch({type: 'UPDATE_PORTFOLIO_DATA', newPortfolioData: res.data})
+    })();
+  }, []);
+
+  return (
+    <section className="PortfolioContainer">
+      {
+        portfolioData ?
+        portfolioData.map((service, index) => {
+          const { _id, imageUrl } = service;
+
+          return (
+            <Work
+              key={index}
+              id={_id}
+              imageUrl={imageUrl}
+            />
+          )
+        }) :
+        null
+      }
+    </section>
+  );
+}
+
+export default WorksContainer;
