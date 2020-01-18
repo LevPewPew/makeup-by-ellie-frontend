ab220c4222cedbd3aa26e290c0cbeaaed0fc7d7d
LevPewPew <levthedev@protonmail.com>
2020-01-18 13:56:12 +1100

    added form and container componenets onto admin dhasboard for 3 different database collections. some of these components don't exist yet

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -2,6 +2,11 @@ import React, { useEffect } from 'react';
 import Axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
 import WorkForm from '../components/WorkForm';
+import TestimonialForm from '../components/TestimonialForm';
+import ServiceForm from '../components/ServiceForm';
+import PortfolioContainer from '../components/PortfolioContainer';
+import TestimonialsContainer from '../components/TestimonialContainer';
+import ServicesContainer from '../components/ServicesContainer';
 
 function AdminPage() {
   const workForm = useSelector((state) => state.form.WorkForm);
@@ -13,8 +18,8 @@ function AdminPage() {
     return () => dispatch({type: "UPDATE_ON_ADMIN_DASH", newOnAdminDash: false});
   }, [])
 
-  async function handleSubmit() {
-    let { category, image } = workForm.values;
+  async function handlePortfolioSubmit() {
+    let { category, imageUrl } = workForm.values;
     let params = { category, image };
     try {
       await Axios.post(`${process.env.REACT_APP_BACKEND_URL}/portfolio`, params);
@@ -23,10 +28,36 @@ function AdminPage() {
     }
   }
 
+  async function handleTestimonialSubmit() {
+    // TODO replace these with testimonial model fields
+    let { name, text } = TestimonialForm.values;
+    let params = { category, image };
+    try {
+      await Axios.post(`${process.env.REACT_APP_BACKEND_URL}/testimonials`, params);
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  }
+
+  async function handleServicesSubmit() {
+    let { name, description, imageUrl } = ServiceForm.values;
+    let params = { category, image };
+    try {
+      await Axios.post(`${process.env.REACT_APP_BACKEND_URL}/services`, params);
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  }
+
   return (
     <div className="AdminPage">
-      <h1>ADMIN PAGE</h1>
-      <WorkForm onSubmit={handleSubmit}/>
+      <h1>Admin Dashboard</h1>
+      <PortfolioContainer />
+      <WorkForm onSubmit={handlePortfolioSubmit}/>
+      <TestimonialsContainer />
+      <TestimonialForm onSubmit={handleTestimonialSubmit}/>
+      <ServicesContainer />
+      <ServiceForm onSubmit={handleServicesSubmit}/>
     </div>
   )
 }
