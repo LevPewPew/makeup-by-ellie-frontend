b43fb38a36cff15dddf3231d9af000066f4c58d7
Pankaj Pawar <pankaj.pawar1986@yahoo.com.au>
2020-01-18 22:09:23 +1100

    Added contact form component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import SandBoxApp from  './components/SandboxApp.jsx';
-import TestComponent from  './components/TestComponent.jsx';
 
-ReactDOM.render(<TestComponent />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
