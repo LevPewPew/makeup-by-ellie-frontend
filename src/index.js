e7608faac735b1ecdb2f632f4d5620b4988c6739
Pankaj Pawar <pankaj.pawar1986@yahoo.com.au>
2020-01-18 22:59:56 +1100

    Added contact from component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-import SandBoxApp from  './components/SandboxApp.jsx';
 
 ReactDOM.render(<App />, document.getElementById("root"));
