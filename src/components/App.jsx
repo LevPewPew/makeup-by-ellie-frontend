ccdcfce9c582cf08881cb74adcf15f4ccded00ec
Martyna Krawczyk <martynakraw@gmail.com>
2020-01-21 10:04:04 +1100

    basic homepage boilerplate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,8 @@ import Navbar from './navbar/Navbar.jsx';
 import HomePage from '../pages/home/HomePage.jsx';
 import ContactForm from '../pages/contact/ContactForm.jsx';
 
+import './App.css';
+
 const store = createStore(rootReducer);
 
 function submit(values)
@@ -22,7 +24,7 @@ function App() {
           <Navbar/>
               <Switch>
                 <Route exact path="/"><HomePage/></Route>
-                <Route exact path="/contact"><ContactForm onSubmit={submit}/></Route>
+                <Route path="/contact"><ContactForm onSubmit={submit}/></Route>
               </Switch>
           </BrowserRouter>
       </div>
