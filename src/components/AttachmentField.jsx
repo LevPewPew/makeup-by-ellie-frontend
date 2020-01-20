56c8eba19e7cd244a356d41a8b540a6adf8447d1
LevPewPew <levthedev@protonmail.com>
2020-01-20 13:14:21 +1100

    front end file attachment and s3 bucket

--- src/components/AttachmentField.jsx
+++ src/components/AttachmentField.jsx
@@ -1,47 +1,48 @@
-import React, { useState } from 'react'
-import { useDropzone } from 'react-dropzone';
- 
-function AttachmentField() {
+import React, { useState, useEffect } from 'react';
+import { useDropzone } from "react-dropzone";
+import './AttachmentField.css';
+
+function AttachmentField(props) {
   const [files, setFiles] = useState([]);
-
-  function onDrop(files) {
-    // console.log(files);
-    setFiles(files);
-  }
-
-  function handleSubmit(event, values) {
-    console.log(values);
-    // const config = { headers: { 'Content-Type': 'multipart/form-data' } };
-    // let fd = new FormData();
-
-    // values.map((file) => {
-    //   fd.append('File[]',file);
-    // });
-
-    // axios.post(`${ROOT_URL}/ImageUpload`, fd, config)
-    //   .then((response) => {
-    //     callback(response);
-    //   })
-    //   .catch(error => {
-    //       errorResponse(error);
-    //   })
-  }
-
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
- 
-  return (
-    <form className="WorkForm" onSubmit={props.handleSubmit}>
-      <div { ...getRootProps() }>
-        <input { ...getInputProps() } />
-        {
-          isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-        }
+  const {
+    input: { onChange }
+  } = props;
+  const {getRootProps, getInputProps} = useDropzone({
+    accept: 'image/*',
+    onDrop: acceptedFiles => {
+      onChange(acceptedFiles);
+      setFiles(acceptedFiles.map(file => Object.assign(file, {
+        preview: URL.createObjectURL(file)
+      })));
+    }
+  });
+  
+  const thumbs = files.map(file => (
+    <div className="thumb" key={file.name}>
+      <div className="thumb-inner">
+        <img
+          src={file.preview}
+        />
       </div>
-      <button type="submit">Submit</button>
-    </form>
-  )
+    </div>
+  ));
+
+  useEffect(() => () => {
+    // Make sure to revoke the data uris to avoid memory leaks
+    files.forEach(file => URL.revokeObjectURL(file.preview));
+  }, [files]);
+
+  return (
+    <section className="container">
+      <div {...getRootProps({className: "Dropzone"})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      <aside className="thumbs-container">
+        {thumbs}
+      </aside>
+    </section>
+  );
 }
 
 export default AttachmentField;
