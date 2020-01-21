fd2514839160f5fa549653dba8508080eab76c87
Martyna Krawczyk <martynakraw@gmail.com>
2020-01-21 14:19:00 +1100

    set up of Lorem ipsum Terms and Privacy pages from footer links

---
+++ src/pages/privacy-policy/PrivacyPolicy.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import './PrivacyPolicy.css';
+
+export default class PrivacyPolicy extends Component {
+  render() {
+    return (
+      <div>
+        <div className="privacy">
+        <p><h2>PRIVACY POLICY</h2><br/>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
+        </p>
+        </div>
+      </div>
+    )
+  }
+}
