dfdf6f76892629171f248159c231d73f3035a4f4
Lev the Dev <levthedev@protonmail.com>
2020-01-22 11:03:32 +1100

    Update ContactsContainer.jsx
    changed css to plural name

--- src/components/ContactsContainer.jsx
+++ src/components/ContactsContainer.jsx
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import Contact from './Contact';
-import './ContactContainer.css'
+import './ContactsContainer.css'
 
 function ContactsContainer() {
 
