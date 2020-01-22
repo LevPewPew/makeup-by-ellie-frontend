7aebb64934046d1f01d79005b8367c3bb40422ad
Pankaj Pawar <pankaj.pawar1986@yahoo.com.au>
2020-01-23 10:01:05 +1100

    Upload form

--- src/pages/imageUpload/Upload.jsx
+++ src/pages/imageUpload/Upload.jsx
@@ -7,15 +7,11 @@ function Upload() {
     console.log(values)
   }
 
-  const onDrop = useCallback(acceptedFiles => {
-    // this callback will be called after files get dropped, we will get the acceptedFiles. If you want, you can even access the rejected files too
-    console.log(acceptedFiles);
-  }, []);
 
   return (
     <div>
       <h1>Test</h1>
-      <UploadForm onDrop={onDrop} onSubmit={submit}/>
+      <UploadForm onSubmit={submit}/>
     </div>
   )
 }
