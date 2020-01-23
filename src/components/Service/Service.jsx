42c38d29bfe829d1c7d765cadb47cce447177014
LevPewPew <levthedev@protonmail.com>
2020-01-23 12:51:33 +1100

    made a test that uses a state from the redux store

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -17,6 +17,7 @@ function Service(props) {
       {
         onAdminDash ?
         <div className="crud-per-doc">
+        <div>TESTLOL</div>
           <BtnEditDoc
             collection={collection}
             _id={_id}
--- src/components/Service/Service.test.js
+++ src/components/Service/Service.test.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Service from './Service';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from '../../reducers/rootReducer';
 
+// const store = createStore(rootReducer);
 
-const store = createStore(rootReducer);
+// TODO get someone to explain this wild syntax
+function renderWithRedux(
+  ui,
+  { initialState, store = createStore(rootReducer, initialState) } = {}
+) {
+  return {
+    ...render(<Provider store={store}>{ui}</Provider>),
+    store,
+  }
+}
 
-test("Service displays title", () => {
-  render(
-    <Provider store={store}>
-      <Service
-        key={1}
-        id={1}
-        title={'DUMMY TITLE'}
-        description={'blah blah blah'}
-        imageUrl={'blob blob blob'}
-      />
-    </Provider>
+test('Service displays title', () => {
+  const { getByText } = renderWithRedux(
+    <Service
+      key={1}
+      id={1}
+      title={'DUMMY TITLE'}
+      description={'blah blah blah'}
+      imageUrl={'blob blob blob'}
+    />, {
+      initialState: { adminDashReducer: { onAdminDash: true } },
+    }
   );
 
-  expect(screen.getByText('DUMMY TITLE')).toBeInTheDocument();
+  expect(getByText('TESTLOL')).toBeInTheDocument();
+  // expect(getByText('DUMMY TITLE')).toBeInTheDocument();
 });
+
+// TODO recreate the basic test lol why did i over write?
