e4cebb5e3018217da88373727b46fd0d6ec31c95
LevPewPew <levthedev@protonmail.com>
2020-01-24 09:06:17 +1100

    created navbar tests

--- src/components/navbar/Navbar.test.jsx
+++ src/components/navbar/Navbar.test.jsx
@@ -3,34 +3,26 @@ import '@testing-library/jest-dom';
 import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
 
-test('Clicking the Services button takes you to the ServicesPage', () => {
+test('Clicking the Contact button takes you to the ContactPage', () => {
+  const { getByTestId } = render(<App />);
+
+  fireEvent.click(getByTestId('nb-link-contact'));
+
+  expect(getByTestId('ContactPage')).toBeInTheDocument();
+});
+
+test('Navbar links cause App to render appropriate page', () => {
   const { getByTestId } = render(<App />);
 
   fireEvent.click(getByTestId('nb-link-services'));
 
   expect(getByTestId('ServicesPage')).toBeInTheDocument();
-});
-
-test('Clicking the FAQ button takes you to the FaqPage', () => {
-  const { getByTestId } = render(<App />);
-  
-  fireEvent.click(getByTestId('nb-link-faq'));
-  
-  expect(getByTestId('FaqPage')).toBeInTheDocument();
-
-  // fireEvent.click(getByTestId('nb-link-contact'));
-
-  // expect(getByTestId('ContactPage')).toBeInTheDocument();
-});
-
-test('Clicking the Contact button takes you to the ContactPage', () => {
-  const { getByTestId } = render(<App />);
-
-  // fireEvent.click(getByTestId('nb-link-faq'));
-  
-  // expect(getByTestId('FaqPage')).toBeInTheDocument();
 
   fireEvent.click(getByTestId('nb-link-contact'));
 
   expect(getByTestId('ContactPage')).toBeInTheDocument();
+
+  fireEvent.click(getByTestId('nb-link-faq'));
+  
+  expect(getByTestId('FaqPage')).toBeInTheDocument();
 });
