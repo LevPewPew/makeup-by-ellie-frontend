268636101ed3e1951f7baf0a9f48ac87806ffcd8
LevPewPew <levthedev@protonmail.com>
2020-01-27 10:49:47 +1100

    rotation fixed, but issues with file overwriting names etc

--- src/components/AttachmentField.jsx
+++ src/components/AttachmentField.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { useDropzone } from 'react-dropzone';
 import './AttachmentField.css';
+const fixRotation = require('fix-image-rotation')
+
+function blobToFile(theBlob, fileName){
+  //A Blob() is almost a File() - it's just missing the two properties below which we will add
+  theBlob.lastModifiedDate = new Date();
+  theBlob.name = `${fileName}.${theBlob.type.split('/')[1]}`;
+  return theBlob;
+}
 
 function AttachmentField(props) {
   const { input, files, setFiles, success, setSuccess } = props;
   const { getRootProps, getInputProps } = useDropzone({
     accept: 'image/*',
-    onDrop: (acceptedFiles) => {
+    onDrop: async (acceptedFiles) => {
+
+      let ArrayOfFilesToBeRotated = acceptedFiles;
+      let myRotationFunction = async function (ArrayOfFilesToBeRotated) {
+        let blobOfArray = await fixRotation.fixRotation(ArrayOfFilesToBeRotated)
+        return blobOfArray
+      }
+      acceptedFiles = await myRotationFunction(ArrayOfFilesToBeRotated);
+      acceptedFiles = acceptedFiles.map((blob, index) => blobToFile(blob, `image-${index}`));
+
       input.onChange(acceptedFiles.concat(files));
       setFiles(acceptedFiles.map(file => Object.assign(file, {
         preview: URL.createObjectURL(file)
