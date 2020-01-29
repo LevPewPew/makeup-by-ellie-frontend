8b12b768858c2f7909bf6a779c73194e8ddea27a
LevPewPew <levthedev@protonmail.com>
2020-01-30 08:05:11 +1100

    updated color of main color as requested by ellie

--- src/components/BtnEditDoc.jsx
+++ src/components/BtnEditDoc.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import _ from 'lodash';
+
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 function BtnEditDoc() {
-
+  const { collection, id, category } = props;
+  const dispatch = useDispatch();
+  
   function handleUpdate() {
-    return 'nothing atm';
+    try {
+      await axios.delete(`${backendUrl}/${collection}/${id}`);
+      let res = await axios.get(`${backendUrl}/${collection}`);
+      dispatch({ type: `UPDATE_${collection.toUpperCase()}_DATA`, [`new${_.upperFirst(collection)}Data`]: res.data });
+      if (category) {
+        dispatch({ type: `${category.toUpperCase()}_PORTFOLIO_DATA` });
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return (
