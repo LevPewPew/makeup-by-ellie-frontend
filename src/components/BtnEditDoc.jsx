36c109e2ffadd1ef8f97a806598166dfc0596882
LevPewPew <levthedev@protonmail.com>
2020-01-30 08:12:14 +1100

    added BtnSubmit component into QuestionForm component

--- src/components/BtnEditDoc.jsx
+++ src/components/BtnEditDoc.jsx
@@ -5,21 +5,23 @@ import _ from 'lodash';
 
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
-function BtnEditDoc() {
+function BtnEditDoc(props) {
   const { collection, id, category } = props;
   const dispatch = useDispatch();
-  
+
   function handleUpdate() {
-    try {
-      await axios.delete(`${backendUrl}/${collection}/${id}`);
-      let res = await axios.get(`${backendUrl}/${collection}`);
-      dispatch({ type: `UPDATE_${collection.toUpperCase()}_DATA`, [`new${_.upperFirst(collection)}Data`]: res.data });
-      if (category) {
-        dispatch({ type: `${category.toUpperCase()}_PORTFOLIO_DATA` });
-      }
-    } catch (err) {
-      console.log(err);
-    }
+    // try {
+    //   await axios.put(`${backendUrl}/${collection}/${id}`, params);
+    //   let res = await axios.get(`${backendUrl}/${collection}`);
+    //   dispatch({ type: `UPDATE_${collection.toUpperCase()}_DATA`, [`new${_.upperFirst(collection)}Data`]: res.data });
+    //   if (category) {
+    //     dispatch({ type: `${category.toUpperCase()}_PORTFOLIO_DATA` });
+    //   }
+    // } catch (err) {
+    //   console.log(err);
+    // }
+
+    // show the editing form here
   }
 
   return (
