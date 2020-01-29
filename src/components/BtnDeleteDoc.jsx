9dae14ab35db748e713e633fc3d2bb068cb6eb4b
LevPewPew <levthedev@protonmail.com>
2020-01-29 15:16:57 +1100

    succesfully refactored for questions related components and FAQ collection

--- src/components/BtnDeleteDoc.jsx
+++ src/components/BtnDeleteDoc.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 import _ from 'lodash';
 
@@ -12,10 +12,7 @@ function BtnDeleteDoc(props) {
   async function handleDelete() {
     try {
       await axios.delete(`${backendUrl}/${collection}/${id}`);
-      (async () => {
-        let res = await axios.get(`${backendUrl}/${collection}`);
-        dispatch({ type: `UPDATE_${collection.toUpperCase()}_DATA`, [`new${_.upperFirst(collection)}Data`]: res.data })
-      })();
+      dispatch({ type: 'SUCCESSFUL_SUBMIT' });
     } catch (err) {
       console.log(err);
     }
--- src/components/QuestionsContainer.jsx
+++ src/components/QuestionsContainer.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Question from './Question';
 import './QuestionsContainer.css'
+import axios from 'axios';
+
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 function QuestionsContainer() {
   const questionsData = useSelector((state) => state.questionsReducer.questionsData);
+  const successfulSubmit = useSelector((state) => state.adminDashReducer.successfulSubmit);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function refreshData() {
+      if (successfulSubmit) {
+        let res = await axios.get(`${backendUrl}/FAQ`);
+        dispatch({ type: 'UPDATE_QUESTIONS_DATA', newQuestionsData: res.data });
+        dispatch({ type: 'NO_SUCCESSFUL_SUBMIT' });
+      }
+    }
+
+    refreshData();
+  }, [successfulSubmit, dispatch]);
 
   return (
     <section className="QuestionsContainer">
