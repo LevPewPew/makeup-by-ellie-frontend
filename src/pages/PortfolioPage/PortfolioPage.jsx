24a195066c17932a08df9c3cdabb81b33fc3205c
Martyna Krawczyk <martynakraw@gmail.com>
2020-01-29 11:54:59 +1100

    sort functionality implemented

--- src/pages/PortfolioPage/PortfolioPage.jsx
+++ src/pages/PortfolioPage/PortfolioPage.jsx
@@ -6,22 +6,10 @@ import './PortfolioPage.css';
 function PortfolioPage() {
   const portfolioData = useSelector((state) => state.portfolioReducer.portfolioData);
 
-
-
-  // const sortData = (category) => {
-  //   portfolioData.filter(img => {
-  //     return img.category === category
-  //   })
-  // }
-
   return portfolioData ? (
     <section className="portfolioGrid">
       <h1>PORTFOLIO</h1>
       <br />
-      <button>Beauty</button>
-      {/* onClick={sortBeauty} */}
-      <button>Editorial</button>
-      <button>Bridal</button>
       <PortfolioContainer />
     </section>
   ) : null
