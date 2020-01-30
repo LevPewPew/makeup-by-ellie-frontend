1f50729fa49538336c82bf093e63b16ae59299bb
Martyna Krawczyk <martynakraw@gmail.com>
2020-01-30 11:14:02 +1100

    style changes as well as some placeholders to form fields

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ContactForm from '../../components/ContactForm';
 import axios from 'axios';
+import homepageImage from '../../media/homepage-image.jpg';
+import './ContactPage.css';
 
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
@@ -15,8 +17,24 @@ class ContactPage extends React.Component {
   render() {
     return (
       <div className="ContactPage" data-testid="ContactPage">
-        <h1>Form Header</h1>
-        <ContactForm onSubmit={this.submit}/>
+        <h1>CONTACT</h1>
+        <div className='sweg'>
+        <tbody className="grid">
+          <td className="formContainer">
+          <ContactForm onSubmit={this.submit}/>
+          </td>
+          <td className="infoContainer">
+          <img src={homepageImage} alt="hero" style={{width: "500px"}}/>
+            <p>
+              <h3>Ellie</h3><br />
+              Ph: xxxx xxx xxx <br /><br />
+              Please complete the form for more information - I will get back to you as soon as possible. <br /><br />
+              Bookings by appointment <br /><br />
+              All appointments for less than four people are located at my private studio in Melbourne. If your party exceeds 3 people, such as for weddings or a special event, I will travel to your required location. <br /><br />
+            </p>
+          </td>
+        </tbody>
+      </div>
       </div>
     )
   }
