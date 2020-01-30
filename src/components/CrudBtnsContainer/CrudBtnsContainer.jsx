fb76d2e818b8a9b1107ed62754eb7de458c84280
LevPewPew <levthedev@protonmail.com>
2020-01-30 12:17:21 +1100

    removed some other unsused vars and files

--- src/components/CrudBtnsContainer/CrudBtnsContainer.jsx
+++ src/components/CrudBtnsContainer/CrudBtnsContainer.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import BtnEditDoc from '../BtnEditDoc/BtnEditDoc';
 import BtnDeleteDoc from '../BtnDeleteDoc/BtnDeleteDoc';
 
 function CrudBtnsContainer(props) {
   const { collection, id } = props;
-
-  const editingForm = useSelector((state) => state.adminDashReducer.editingForm);
 
   const token = localStorage.getItem('token');
 
