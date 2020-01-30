f9e19acfa8e8f2ded6ba94ac595015692aa16b50
LevPewPew <levthedev@protonmail.com>
2020-01-30 18:32:01 +1100

    created submit handler for services and removed services form from admin page placed into form container

--- src/components/BtnCreateDoc/BtnCreateDoc.jsx
+++ src/components/BtnCreateDoc/BtnCreateDoc.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import _ from 'lodash';
 
 function BtnCreateDoc(props) {
   const { collection } = props;
@@ -11,7 +12,7 @@ function BtnCreateDoc(props) {
   }
 
   return (
-    <button className="BtnCreateDoc btn" onClick={renderCreateForm}>{`New ${collection} Item`}</button>
+    <button className="BtnCreateDoc btn" onClick={renderCreateForm}>{`New ${_.upperFirst(collection)} Item`}</button>
   )
 }
 
