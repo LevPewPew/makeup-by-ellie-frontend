2fbb133b2887fb40163ab39484349d3e123c91fc
LevPewPew <levthedev@protonmail.com>
2020-02-02 15:50:58 +1100

    changed class componenets to functional

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -6,39 +6,39 @@ import './ContactPage.scss';
 
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
-class ContactPage extends React.Component {
+function ContactPage() {
+  function submit(values) {
+    try {
+      let res = axios.post(`${backendUrl}/contact`, values);
+      alert(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-   submit = (values) => {
-      axios.post(`${backendUrl}/contact`,values)
-      .then(res=>alert(res.data))
-      .catch(err=> console.log(err))
-   }
-
-  render() {
-    return (
-      <div className="ContactPage" data-testid="ContactPage">
-        <h1>CONTACT</h1>
-        <div className="sweg">
-          <div className="grid">
-            <div className="formContainer">
-            <ContactForm onSubmit={this.submit}/>
-            </div>
-            <div className="infoContainer">
+  return (
+    <div className="ContactPage" data-testid="ContactPage">
+      <h1>CONTACT</h1>
+      <div className="sweg">
+        <div className="grid">
+          <div className="formContainer">
+            <ContactForm onSubmit={submit}/>
+          </div>
+          <div className="infoContainer">
             <img src={homepageImage} alt="hero" style={{ width: "400px" }}/>
             <h3>Ellie</h3><br />
-              <p>
-                Ph: xxxx xxx xxx <br /><br />
-                Sample Information <br /><br />
-                Please complete the form for more information - I will get back to you as soon as possible. <br /><br />
-                Bookings by appointment <br /><br />
-                All appointments for less than four people are located at my private studio in Melbourne. If your party exceeds 3 people, such as for weddings or a special event, I will travel to your required location. <br /><br />
-              </p>
-            </div>
+            <p>
+              Ph: xxxx xxx xxx <br /><br />
+              Sample Information <br /><br />
+              Please complete the form for more information - I will get back to you as soon as possible. <br /><br />
+              Bookings by appointment <br /><br />
+              All appointments for less than four people are located at my private studio in Melbourne. If your party exceeds 3 people, such as for weddings or a special event, I will travel to your required location. <br /><br />
+            </p>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default ContactPage;
--- src/pages/privacy-policy/PrivacyPolicy.jsx
+++ src/pages/privacy-policy/PrivacyPolicy.jsx
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './PrivacyPolicy.scss';
 
-class PrivacyPolicy extends Component {
-  render() {
-    return (
-      <div className="PrivacyPolicy">
-        <h2>PRIVACY POLICY</h2>
-        <br />
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
-        </p>
-      </div>
-    )
-  }
+function PrivacyPolicy() {
+  return (
+    <div className="PrivacyPolicy">
+      <h2>PRIVACY POLICY</h2>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum nesciunt odio reiciendis numquam, harum repudiandae repellendus fuga cumque dolore maiores beatae. Dolorem doloribus iste corrupti veritatis voluptate provident repellat sequi!
+      </p>
+    </div>
+  )
 }
 
 export default PrivacyPolicy;
