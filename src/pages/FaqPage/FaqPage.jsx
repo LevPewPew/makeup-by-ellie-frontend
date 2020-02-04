bc98936ecad83b1736f794998d89f442fef4eda7
Pankaj Pawar <pankaj.pawar1986@yahoo.com.au>
2020-02-05 10:44:45 +1100

    Added styling to FAQ page

--- src/pages/FaqPage/FaqPage.jsx
+++ src/pages/FaqPage/FaqPage.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import QuestionsContainer from '../../components/QuestionsContainer/QuestionsContainer';
 import './FaqPage.scss';
 
 function FaqPage() {
   return (
     <div className="FaqPage" data-testid="FaqPage">
-      <div className="FaqText">
-        <h1>FAQ</h1>
-        <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam sunt eaque omnis? Molestias facere magni dicta iste minima, itaque voluptates saepe voluptatibus accusantium, sed, eaque officiis impedit eveniet quasi laboriosam!
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam sunt eaque omnis? Molestias facere magni dicta iste minima, itaque voluptates saepe voluptatibus accusantium
-        </p>
+      <div  className="FaqText">
+        <h1>Frequently Asked Questions</h1>
+        <p>Here you will find answers to the questions i get asked most.</p>
       </div>
-      <div className="FaqContainer">
+      <div>
         <QuestionsContainer/>
-      </div>     
+      </div>    
+      <p>If you have any questions, please contact me using below link.<br/><Link to="/contact" style={{textDecoration:"underline"}}>Contact Me</Link></p> 
     </div>
   );
 }
