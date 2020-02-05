b47af458f0a55e6a3505d5aa4fbb0de92b836d83
LevPewPew <levthedev@protonmail.com>
2020-02-05 12:01:48 +1100

    save point

--- src/pages/AdminLoginPage/AdminLoginPage.jsx
+++ src/pages/AdminLoginPage/AdminLoginPage.jsx
@@ -1,29 +1,29 @@
 import React, { useState } from 'react';
 import AdminLoginForm from '../../components/AdminLoginForm/AdminLoginForm';
-import {useSelector,useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Redirect } from 'react-router';
 import axios from 'axios';
+import './AdminLoginPage.scss';
 
 function AdminLoginPage() {
   const [ redirect, setRedirect ] = useState(false);
 
-  const token = useSelector((state)=> state.tokenReducer.token);
   const dispatch = useDispatch();
 
   const submit = async (values) => {
     try {
       const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/admin-login`, { username: values.username, password: values.password });
       localStorage.setItem('token', response.data.token);
-      dispatch({type:"UPDATE_TOKEN_DATA",newToken: response.data.token})
+      dispatch({ type: "UPDATE_TOKEN_DATA", newToken: response.data.token });
       setRedirect(true);
     } catch {
-      alert("Incorrect credentials. Please try again")
+      alert("Incorrect credentials. Please try again");
     }
   }
 
   return (
     <div className="AdminLoginPage">
-      <h3>Admin Login</h3>
+      <h1>ADMIN LOGIN</h1>
       <AdminLoginForm onSubmit={submit} />
       {
         redirect ?
