8fcc9fef6271f6ea060b15bb740e3abbdafec237
LevPewPew <levthedev@protonmail.com>
2020-02-06 12:44:31 +1100

    TnC and Privacy policy styled

---
+++ src/pages/TermsConditions/TermsConditions.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './TermsConditions.scss';
+function TermsConditions(){
+  return (
+    <div className="TermsConditions">
+      <h2>Terms and Conditions</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla labore eaque optio obcaecati tempora rem ipsam placeat natus perferendis quam libero iure neque, inventore quasi quas voluptates facilis, vel sequi!<br /><br />
+        Perspiciatis obcaecati modi, vitae voluptatum harum officia deleniti maiores minima eveniet. Iusto cupiditate delectus porro, odio, corrupti maiores optio, quos non ipsum temporibus ratione quis eum in itaque. Recusandae, quibusdam.<br /><br />
+        Aliquam temporibus officiis quis rerum inventore tempora, alias ad expedita facere porro ea soluta enim deleniti ut quae veniam repudiandae est illum adipisci. Vero dolores explicabo dolorem corrupti amet enim.<br /><br />
+        Neque, culpa. Delectus corrupti, sit ea facilis eaque veniam vero accusamus quasi tempora nihil aliquam officia incidunt labore nemo qui nulla voluptates. Ut ducimus atque in facilis cum neque debitis!<br /><br />
+        Pariatur, quis corporis ut nulla error alias! Reiciendis deserunt sit vel quisquam incidunt perspiciatis sed sunt culpa possimus molestias, optio, veniam, ad nobis! Vitae adipisci eaque saepe expedita aut dolorum!<br /><br />
+        Commodi quidem, dolor quisquam modi, quia perspiciatis, aliquam minima at soluta a culpa nemo in iste veniam! Voluptas neque natus, beatae sint, quod expedita porro, rem illum accusamus perferendis veritatis.<br /><br />
+        Deleniti consequuntur numquam non necessitatibus exercitationem, eveniet harum, temporibus et quasi, repellat repellendus culpa? Exercitationem ratione ullam nesciunt iure, odit nam dolore officiis inventore, quaerat in autem, nemo dolorem dolores.<br /><br />
+        Aliquid molestias distinctio architecto, accusantium dignissimos vel autem quis tempore iste in nisi ad illum, exercitationem rerum iure error ipsam quas molestiae suscipit neque saepe eum quod similique dolore. Iusto.<br /><br />
+        Eius voluptatum, tenetur quae dolor fugit cumque? Nam quibusdam quod numquam similique perferendis magni eaque. Molestias, doloremque. Fugiat harum mollitia error, eaque magnam eius nesciunt, eveniet fugit iusto delectus molestiae.<br /><br />
+        Aliquam earum iste fugiat natus, quia possimus nostrum voluptatem similique cumque itaque rerum odit ea, ab fuga. Facere vel, recusandae sit asperiores ipsum temporibus illo beatae neque cum modi harum!
+      </p>
+    </div>
+  );
+}
+
+export default TermsConditions;
