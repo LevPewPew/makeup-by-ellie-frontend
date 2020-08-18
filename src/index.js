9f094051e4c6ffd1f7e776fb63162e0e2cf91ea8
LevPewPew <levthedev@protonmail.com>
2020-08-18 19:59:22 +1000

    made frontpage fully responsive

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './reducers/rootReducer';
 import App from './components/App';
 import store from './store';
 import * as serviceWorker from './serviceWorker';
