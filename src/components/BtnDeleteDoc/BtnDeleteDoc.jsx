21e6bfe59a5f9efef3280ccdd287ba230b466dd7
LevPewPew <levthedev@protonmail.com>
2022-04-14 18:49:17 +1000

    hacky delete prompt added

--- src/components/BtnDeleteDoc/BtnDeleteDoc.jsx
+++ src/components/BtnDeleteDoc/BtnDeleteDoc.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 import _ from 'lodash';
 
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
+function DeletePrompt({ handleDelete, setDeletePromptShown }) {
+  return (
+    <div
+      style={{
+        backgroundColor: 'cyan',
+        width: 300,
+        height: 300,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'centre',
+        justifyContent: 'space-between',
+      }}
+    >
+      <button style={{ backgroundColor: 'red' }} onClick={handleDelete}>
+        YES, DELETE
+      </button>
+      <button onClick={() => setDeletePromptShown(false)}>No, my bad</button>
+    </div>
+  );
+}
 
 function BtnDeleteDoc(props) {
   const { collection, id, category } = props;
@@ -11,12 +32,18 @@ function BtnDeleteDoc(props) {
   const createForm = useSelector((state) => state.adminDashReducer.createForm);
   const editingForm = useSelector((state) => state.adminDashReducer.editingForm);
   const dispatch = useDispatch();
+  const [isDeletePromptShown, setDeletePromptShown] = useState(false);
 
   async function handleDelete() {
+    setDeletePromptShown(false);
+
     try {
       await axios.delete(`${backendUrl}/${collection}/${id}`);
       let res = await axios.get(`${backendUrl}/${collection}`);
-      dispatch({ type: `UPDATE_${collection.toUpperCase()}_DATA`, [`new${_.upperFirst(collection)}Data`]: res.data });
+      dispatch({
+        type: `UPDATE_${collection.toUpperCase()}_DATA`,
+        [`new${_.upperFirst(collection)}Data`]: res.data,
+      });
       if (category) {
         dispatch({ type: `FILTER_PORTFOLIO_DATA_${category.toUpperCase()}` });
       }
@@ -26,7 +53,21 @@ function BtnDeleteDoc(props) {
   }
 
   return (
-    <button className="BtnDeleteDoc btn" type="submit" onClick={handleDelete} disabled={editingForm || createForm} data-testid="BtnDeleteDoc">Delete</button>
+    <>
+      {isDeletePromptShown ? (
+        <DeletePrompt handleDelete={handleDelete} setDeletePromptShown={setDeletePromptShown} />
+      ) : (
+        <button
+          className="BtnDeleteDoc btn"
+          type="submit"
+          onClick={() => setDeletePromptShown(true)}
+          disabled={editingForm || createForm}
+          data-testid="BtnDeleteDoc"
+        >
+          Delete
+        </button>
+      )}
+    </>
   );
 }
 
